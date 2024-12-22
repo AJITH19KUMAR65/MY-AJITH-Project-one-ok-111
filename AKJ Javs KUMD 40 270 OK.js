bb06ca5a0311c2b1f397d37e4526c3d2969b68ce
AJITH19KUMAR65 <kums973@gmail.com>
2024-12-22 17:53:17 +0530

    Add files via upload

---
+++ AKJ Javs KUMD 40 270 OK.js
@@ -0,0 +1,109 @@
+console.log(1234);
+console.log("welcome");
+console.log(1.15)
+console.log(true);
+console.log(1,2,3,4,);
+console.log((name;"manoj"));
+
+
+
+var a =10;
+var b = "welcome";
+console.log(a+b); .1010.
+
+b = Number(b)
+console.log(a+b); 20
+
+a = String(a):
+console.log(typeof a);
+console.log(a+b); ;10 10 ;
+
+
+
+if (true) {
+    var let  msg1 ="welcome" 
+            
+
+
+if (true) {
+     const msg1 ="welcome today class";
+     console.log(msg1); welcome today class
+}
+
+
+
+console.log(msg1); welcome
+
+  var x =10
+console.log(x); 10
+var x =100
+ console.log(x); 100
+ 
+
+
+let number ={1,2,3,4,5,6,7,8,9}
+console.log{number}; Array(9) {1, 2, 3, 4, 5, 6, 7, 8, 9,}
+
+number.fill(50);
+console.log(number); Array {50, 50, 50, 50, 50, 50, 50, 50, 50,}
+
+number.fill(100,4,7,); 
+console.log(number) ; Array(9) 50, 50, 50, 50, 100, 100, 100, 50, 50,}
+
+
+function callback()[
+   console.log(welcome); welcome
+]
+\\ callback()
+
+function test (value){ 
+      value()
+)
+test(callback)
+
+let english = 80, tamil = 90, cs = 85;
+let total,avg;
+
+total = english + tamil +  cs ;
+avg = total\3;
+
+console.log("Total is :" + total);"Total is :255"
+console.log("Avg is " + avg);  Avg is 85
+
+if(avg>= 91 && )
+
+if(english >= 35 && tamil>= 35 && cs >=35){
+console.log("Result is  pass")
+else{ if (avg >= 81 && avg <= 90 ){ 
+console.log("Grade :B ");         " Gname " : B
+
+
+
+
+
+else
+console.log("Nothinme");
+
+
+
+
+}
+else{ 
+    console.log(" Result is fail "); "Resuit is fail "
+console.log("No Grade");  " No Grade"
+
+
+HashChangeEvent
+
+
+
+KAUJAHA 20AGAHHA 98 
+
+
+
+KUM1 14.05.2025 FTDFFB
+
+
+
+
+
